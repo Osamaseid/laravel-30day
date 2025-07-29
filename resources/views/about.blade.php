2e39361cc65870fa024c975e31123f2fe6eb7040
osama seid <osaseid1@gmail.com>
2025-07-29 10:21:39 +0300

    laravel 30 days challenge

---
+++ resources/views/about.blade.php
@@ -0,0 +1,6 @@
+<x-layout>
+    <x-slot:heading>
+        <h1 class="text-3xl font-bold leading-tight text-gray-900">About Page</h1>
+    </x-slot:heading>
+    <p class="mt-4 text-gray-600">This is the about page of our application. Here you can find information about our mission, vision, and team.</p>
+</x-layout>
---
+++ resources/views/contact.blade.php
@@ -0,0 +1,7 @@
+<x-layout>
+    <x-slot:heading>
+        <h1 class="text-3xl font-bold leading-tight text-gray-900">Contact Page</h1>
+    </x-slot:heading>
+    <p class="mt-4 text-gray-600">This is the contact page of our application. Here you can find our contact information and a form to reach out to us.</p>
+
+</x-layout>
