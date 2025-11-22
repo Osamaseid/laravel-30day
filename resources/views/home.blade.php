77d6de71ec39e722d5afe2c6ead149602eaaaca1
osama <osaseid1@gmail.com>
2025-11-22 23:41:57 +0300

    project removed

--- resources/views/home.blade.php
+++
@@ -1,7 +0,0 @@
-<x-layout>
-    <x-slot:heading>
-        <h1 class="text-3xl font-bold leading-tight text-gray-900">Home Page</h1>
-    </x-slot:heading>
-    <p class="mt-4 text-gray-600">Welcome to the home page of our application. Here you can find the latest updates and features.</p>
-
-</x-layout>
